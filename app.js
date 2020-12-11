f29a8b9e447f1f66a2fcf8ec03faaa256defb0b4
Helton Isac <helton.isac@gmail.com>
2020-12-10 21:46:59 -0300

    users and login routes

--- app.js
+++ app.js
@@ -55,11 +55,55 @@ app.get("/admin", cors(optionsConfig), (req, res) => {
   res.send(req.content);
 });
 
-app.get("/admin/users", cors(optionsConfig), autentica, (req, res) => {});
+app.get("/admin/users", cors(optionsConfig), autentica, (req, res) => {
+  if (req.content.usuario != "veronica") {
+    res
+      .status(403)
+      .send({ rs: "Você não está autorizado a ver o conteudo desta página" });
+    return;
+  }
+  cliente
+    .find((err, dados) => {
+      if (err) {
+        res.status(400).send({ rs: "Não há usuários" });
+        return;
+      }
+      res.status(200).send({ rs: dados });
+    })
+    .select([
+      "-cpf",
+      "-telefone",
+      "-idade",
+      "-senha",
+      "-datacadastro",
+      "-___v",
+    ]);
+});
 
 app.get("/admin/clientes", cors(optionsConfig), autentica, (req, res) => {});
 
-app.post("/login", cors(optionsConfig), (req, res) => {});
+app.post("/login", cors(optionsConfig), (req, res) => {
+  const usuario = req.body.usuario;
+  const senha = req.body.senha;
+  cliente.findOne({ usuario: usuario }, (err, data) => {
+    if (err) {
+      res.status(400).send({ rs: `Erro ao buscar o usuário: ${err}` });
+      return;
+    }
+    if (!dados) {
+      res.status(400).send({ rs: `Não há usuários cadastrados` });
+      return;
+    }
+    bcrypt.compare(senha, data.senha, (err, mesma) => {
+      if (!mesma) {
+        res.send({ erro: "Erro ao autenticar o usuário" });
+        return;
+      }
+      const token = createUserToken(data._id, data.usuario, data.nome);
+      res.status(200).send({ rs: "Usuário Autenticado", token: token });
+    });
+  });
+});
 
 app.post("/cadastro", cors(optionsConfig), (req, res) => {
   //validação de cpf e email
